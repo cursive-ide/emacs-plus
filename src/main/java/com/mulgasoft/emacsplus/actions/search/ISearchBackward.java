06d3ebc32c5c26d2ec733e2f1cef6a5262e1f560
Colin Fleming <colin@colinfleming.net>
2020-02-26 14:02:13 +1300

    Fix to support 2019.3 Fixes https://github.com/cursive-ide/emacs-plus/issues/8 Fixes https://github.com/cursive-ide/emacs-plus/issues/13 Fixes https://github.com/cursive-ide/emacs-plus/issues/14 Fixes https://github.com/cursive-ide/emacs-plus/issues/15 Fixes https://github.com/cursive-ide/emacs-plus/issues/16 Fixes https://github.com/cursive-ide/emacs-plus/issues/17

--- src/main/java/com/mulgasoft/emacsplus/actions/search/ISearchBackward.java
+++ src/main/java/com/mulgasoft/emacsplus/actions/search/ISearchBackward.java
@@ -1,34 +1,57 @@
 package com.mulgasoft.emacsplus.actions.search;
 
 import com.intellij.find.FindUtil;
-import com.intellij.ide.actions.SearchBackAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.editor.actionSystem.EditorAction;
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import com.intellij.openapi.project.Project;
 import com.mulgasoft.emacsplus.util.ActionUtil;
 import com.mulgasoft.emacsplus.util.EmacsIds;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ISearchBackward extends SearchBackAction {
-  @Override
-  public void actionPerformed(AnActionEvent e) {
-    delegateAction(e);
+import static com.intellij.openapi.editor.actions.IncrementalFindAction.SEARCH_DISABLED;
+
+public class ISearchBackward extends EditorAction {
+  protected ISearchBackward() {
+    this(new Handler());
   }
 
-  protected static ISearch delegateAction(AnActionEvent e) {
-    Editor editor = FileEditorManager.getInstance(e.getProject()).getSelectedTextEditor();
+  protected ISearchBackward(EditorActionHandler handler) {
+    super(handler);
+  }
+
+  protected static class Handler extends EditorActionHandler {
+    @Override
+    protected void doExecute(@NotNull Editor editor, @Nullable Caret caret, DataContext dataContext) {
+      ISearch searcher = findSearcher(editor, dataContext);
+      if (searcher != null) {
+        searcher.searchBackward();
+      }
+    }
+
+    @Override
+    protected boolean isEnabledForCaret(@NotNull Editor editor, @NotNull Caret caret, DataContext dataContext) {
+      Project project = dataContext.getData(CommonDataKeys.PROJECT);
+      if (project == null) {
+        return false;
+      }
+      return !editor.isOneLineMode() && !SEARCH_DISABLED.get(editor, false);
+    }
+  }
+
+  protected static ISearch findSearcher(@NotNull Editor editor, DataContext dataContext) {
     ISearch searcher = ISearch.from(editor);
     if (searcher == null) {
-      ActionUtil.dispatch(EmacsIds.ISEARCH_ID, e.getDataContext());
+      ActionUtil.dispatch(EmacsIds.ISEARCH_ID, dataContext);
       searcher = ISearch.from(editor);
     } else {
       searcher.requestFocus();
       FindUtil.configureFindModel(false, editor, searcher.getFindModel(), false);
     }
-
-    if (searcher != null) {
-      searcher.searchBackward();
-    }
-
     return searcher;
   }
 }
--- src/main/java/com/mulgasoft/emacsplus/actions/search/ISearchBackwardRegexp.java
+++ src/main/java/com/mulgasoft/emacsplus/actions/search/ISearchBackwardRegexp.java
@@ -1,14 +1,24 @@
 package com.mulgasoft.emacsplus.actions.search;
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Editor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ISearchBackwardRegexp extends ISearchBackward {
-  @Override
-  public void actionPerformed(AnActionEvent e) {
-    ISearch searcher = delegateAction(e);
-    if (searcher != null) {
-      searcher.getFindModel().setRegularExpressions(true);
-    }
+  public ISearchBackwardRegexp() {
+    super(new RegexpHandler());
+  }
 
+  private static class RegexpHandler extends ISearchBackward.Handler {
+    @Override
+    protected void doExecute(@NotNull Editor editor, @Nullable Caret caret, DataContext dataContext) {
+      ISearch searcher = findSearcher(editor, dataContext);
+      if (searcher != null) {
+        searcher.getFindModel().setRegularExpressions(true);
+        searcher.searchBackward();
+      }
+    }
   }
 }
